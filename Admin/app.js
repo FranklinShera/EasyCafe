8035956e62f3f518ef2803014d5a6c58e5fe8b56
kiprono Denis <kipronokemei98@gmail.com>
2019-05-16 06:29:58 +0300

    Conected users page to the database

--- Admin/app.js
+++ Admin/app.js
@@ -1,6 +1,4 @@
-
 const e=React.createElement;
-
 //users section
 
 
@@ -25,41 +23,8 @@ class User extends React.Component {
                 ]
 
 
-        },
-        {
-          id:2,
-          image:"images/user.jpg",
-          data:[
-        {Name:"Name:",value:"Kiptanui Reliph",style:{}},
-        {Name:"Email:",value:"reliphkiptanui98@gmail",style:{}},
-        {Name:"Customers served:",value:200,style:{}},
-        {Name:"Status:",value:"Active",style:{color:"#10E37D"}},
-        {Name:"Date Added:",value:"01/12/19",style:{}},
-        {Name:"Role:",value:"User",style:{color:"#BB3131"}},
-        {Name:"Last Login:",value:"12/2/19",style:{color:"#999999",fontWeight:" 100",fontSize:"16px"}}
-
-                   
-                ]
-
-
-        },
-        {
-          id:3,
-          image:"images/user.jpg",
-          data:[
-        {Name:"Name:",value:"Kipchirchir Felix",style:{}},
-        {Name:"Email:",value:"reliphkiptanui98@gmail",style:{}},
-        {Name:"Customers served:",value:800,style:{}},
-        {Name:"Status:",value:"Active",style:{color:"#10E37D"}},
-        {Name:"Date Added:",value:"15/12/18",style:{}},
-        {Name:"Role:",value:"User",style:{color:"#BB3131"}},
-        {Name:"Last Login:",value:"Active now",style:{color:"#999999",fontWeight:" 100",fontSize:"16px"}}
-
-                   
-                ]
-
-
         }
+       
         
         ]
       }//end of state
@@ -67,15 +32,25 @@ class User extends React.Component {
 
     }//end of constructor
 
+componentDidMount() {
+   fetch("http://localhost/Denis/Admin/users.php")
+   .then(response=>response.json())
+   .then(data=>{
+    let changedS=this.state;
+    changedS.data=data;
+    this.setState(changedS);
+
+   })
+}
     render(){
 
       return(
-          e("div",{className:"card-deck  mx-2 text-center"},
+          e("div",{className:"deck mx-2 text-center "},
 
              this.state.data.map(item=>{
-          return  e("div",{className:"card",key:item.id},
+          return  e("div",{className:"card col-md-4 mx-auto",key:item.id},
 
-           e("img",{src:item.image,className:"card-img-top"}),
+           e("img",{src:item.image,className:"card-img-top user_card"}),
            e("div",{className:"card-body mt-3 px-0",style:{backgroundColor:"white !importart",border:"none !important"}},
             e("ul",{className:"list-group"},
               item.data.map(data=>{
@@ -86,7 +61,7 @@ class User extends React.Component {
                     )
               }),
               e("li",{},e("a",{href:"#",className:"btn btn-danger float-right m-2"},"Delete"),
-                e("a",{href:"#",className:"btn btn-success float-left m-2"},"Edit")
+                e("a",{href:"#",className:"btn btn-success float-left m-2",onClick:()=>this.props.click(6)},"Edit")
                 ),
               
               )
@@ -696,11 +671,11 @@ const Update=(item)=> {
 
 
 	switch(data) {
-        	case 1:console.log("Iam the first");
+        	case 1:
         	return(e(Body))
         	break;
-        	case 2:console.log("Iam the second");
-        	return(e(User))
+        	case 2:
+        	return(e(User,{click:Update}))
         	break;
         	case 3:console.log("Iam the third and default");
         	return(e(Product))
@@ -711,6 +686,9 @@ const Update=(item)=> {
         	case 5:
         	return(e(PromotionB))
         	break;
+          case 6:
+          return(e("div",null,"Hallo world"))
+          break;
         	default:
         	 return(e(Body))
         	 break;
---
+++ Admin/users.php
@@ -0,0 +1,128 @@
+<?php
+include_once '../connection.php';
+
+$all=[];
+$data=[
+// array(
+// "id"=>"1",
+// "image"=>"images/user.jpg",
+// "data"=>array(
+// array(
+// 	"Name"=>"Name:",
+// 	"value"=>"Kiprono Denis",
+// 	"style"=>array()
+
+// ),
+// array(
+// 	"Name"=>"Email:",
+// 	"value"=>"kipronokemei98@gmail",
+// 	"style"=>array()
+
+// ),
+// array(
+// "Name"=>"Customers served:",
+// 	"value"=>"1200",
+// 	"style"=>array()
+// ),
+// array(
+// "Name"=>"Status:",
+// 	"value"=>"Active",
+// 	"style"=>array("color"=>"#10E37D")
+// ),
+// array(
+// "Name"=>"Date Added:",
+// 	"value"=>"12/12/18",
+// 	"style"=>array()
+// ),
+// array(
+// "Name"=>"Role:",
+// 	"value"=>"Admin",
+// 	"style"=>array("color"=>"#BB3131")
+// ),
+// array(
+// "Name"=>"Last Login:",
+// 	"value"=>"12/2/19",
+// 	"style"=>array("color"=>"#999999",
+//                    "fontWeight"=>" 100",
+//                    "fontSize"=>"16px"
+//                   )
+
+// )
+
+// )
+// )
+
+
+];
+
+
+//echo json_encode($data);
+
+	$sql="SELECT * , (select count(*) FROM customers where server_id=users.id) as total,(SELECT login FROM `user_logs` WHERE id=(SELECT MAX(id) FROM user_logs WHERE user_id=users.id)) as lastLogin FROM users";
+	$result=$con->query($sql);
+	if($row=$result->fetch_assoc()) {
+		$users[]=$row;
+	}
+
+	function details($user) {
+
+	$itemN['Name']="Name:";
+	$itemN['value']=$user['firstname']. " ".$user['lastname'];
+	$itemN['style']=array();
+	$result[]=$itemN;
+
+	//Email
+		$itemE['Email']="Email:";
+	$itemE['value']=$user['email'];
+	$itemE['style']=array();
+	$result[]=$itemE;
+//customers served
+   	$itemC['Name']="Customers served:";
+	$itemC['value']=$user['total'];
+	$itemC['style']=array();
+	$result[]=$itemC;
+
+	//STATUS 
+
+	if(isset($_SESSION['username'])) {
+		$status=$_SESSION['username']==$user['username']?"Active":"Not Active";
+	}else {
+		$status="NOT ACTIVE";
+	}
+	$itemS['Name']="Status:";
+	$itemS['value']=$status;
+	$itemS['style']=array("color"=>"#10E37D");
+	$result[]=$itemS;
+  //Date added
+	$itemD['Name']="Date Added:";
+	$itemD['value']=$user['created_at'];
+	$itemD['style']=array();
+	$result[]=$itemD;
+//ROLE
+  $itemR['Name']="Role: ";
+	$itemR['value']=$user['role'];
+	$itemR['style']=array("color"=>"#BB3131");
+	$result[]=$itemR;
+//last Login
+	$itemL['Name']="LastLogin:";
+	$itemL['value']=$user['lastLogin'];
+	$itemL['style']=array("color"=>"#999999",
+                   "fontWeight"=>" 100",
+                   "fontSize"=>"16px"
+                  );
+	$result[]=$itemL;
+	return $result;
+	}
+
+	foreach($users as $user) {
+		$data['id']=$user['id'];
+		$data['image']="images/user.jpg";
+	   $data['data']=details($user);
+	   array_push($all, $data);
+	}
+
+
+
+echo json_encode($all);
+
+ ?>
